1bdfc0fec9db01d0bc1508ef06f9e5fc2f4afd4b
Emilly Trevisan <emillyvivit@gmail.com>
2020-10-15 14:22:41 -0300

    adicionando programa calculadora c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale (LC_ALL,"");
+    float n1, n2, soma, subtracao, vezes, divisao;
+    char op;
+    printf("Calculadora operações básicas\n\n");
+    printf("Digite: número op número\n\n");
+    scanf("%f %c %f", &n1, &op, &n2);
+    soma=n1+n2;
+    subtracao=n1-n2;
+    vezes=n1*n2;
+    divisao=n1/n2;
+    switch (op)
+    {
+    case '+':
+        printf("A soma dos números é %.2f\n",soma);
+    break;
+    case '-':
+        printf("A subtração dos números é %.2f\n", subtracao);
+    break;
+    case '*':
+        printf("A multiplicação dos números é %.2f\n", vezes);
+    break;
+    case '/':
+        printf("A divisão dos números é %.2f\n", divisao);
+    break;
+    }
+
+    return 0;
+}
